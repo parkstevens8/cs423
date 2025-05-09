79f06de06d75bcf70742376b405160f8a2d1c584
Parker Stevenson <pjstevenson2@crimson.ua.edu>
2025-05-09 16:50:00 -0700

    adding NaN values

--- library.py
+++ library.py
@@ -716,8 +716,8 @@ customer_variance_based_split = 113
 
 #first define the pipeline
 titanic_transformer = Pipeline(steps=[
-    ('map_gender', CustomMappingTransformer('Gender', {'Male': 0, 'Female': 1})),
-    ('map_class', CustomMappingTransformer('Class', {'Crew': 0, 'C3': 1, 'C2': 2, 'C1': 3})),
+    ('map_gender', CustomMappingTransformer('Gender', {'Male': 0, 'Female': 1, np.nan: -1})),
+    ('map_class', CustomMappingTransformer('Class', {'Crew': 0, 'C3': 1, 'C2': 2, 'C1': 3, np.nan: -1})),
     ('target_joined', CustomTargetTransformer(col='Joined', smoothing=10)),
     ('tukey_age', CustomTukeyTransformer(target_column='Age', fence='outer')),
     ('tukey_fare', CustomTukeyTransformer(target_column='Fare', fence='outer')),
