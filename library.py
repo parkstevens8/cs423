eb2d6d4f8952c7ea383fd2ca706ff0f87bd6a648
Parker Stevenson <pjstevenson2@crimson.ua.edu>
2025-05-18 17:49:30 -0700

    adding chapter 11 functions

--- library.py
+++ library.py
@@ -17,6 +17,7 @@ import sklearn
 import warnings
 from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score, roc_auc_score
 from sklearn.linear_model import LogisticRegressionCV
+from sklearn.model_selection import ParameterGrid
 
 # This sets built-in transformers to output pandas DataFrames
 sklearn.set_config(transform_output="pandas")
@@ -803,3 +804,32 @@ def threshold_results(thresh_list, actuals, predicted):
 
   fancy_df = result_df.style.highlight_max(color = 'pink', axis = 0).format(precision=2).set_properties(**properties).set_table_styles([headers])
   return (result_df, fancy_df)
+
+def halving_search(model, grid, x_train, y_train, factor=2, min_resources="exhaust", scoring='roc_auc'):
+  #your code below
+  halving_cv = HalvingGridSearchCV(
+      model, grid,
+      scoring=scoring,
+      n_jobs=-1,
+      min_resources=min_resources,
+      factor=factor,
+      cv=5,
+      random_state=1234,
+      refit=True
+  )
+
+  grid_result = halving_cv.fit(x_train, y_train)
+
+  return grid_result
+
+def sort_grid(grid):
+  sorted_grid = grid.copy()
+
+  #sort values - note that this will expand range for you
+  for k,v in sorted_grid.items():
+    sorted_grid[k] = sorted(sorted_grid[k], key=lambda x: (x is None, x))  #handles cases where None is an alternative value
+
+  #sort keys
+  sorted_grid = dict(sorted(sorted_grid.items()))
+
+  return sorted_grid
